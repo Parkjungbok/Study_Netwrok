5d14badce7099fb1547ac7e19bc641b6ddc5e107
sumeing.vogi@gmail.com <sumeing.vogi@gmail.com>
2024-04-26 16:19:58 +0900

    feat : 수업내용

--- Assets/Game/PlayerController.cs
+++ Assets/Game/PlayerController.cs
@@ -1,32 +1,69 @@
 using Photon.Pun;
+using Photon.Pun.UtilityScripts;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPun, IPunObservable
 {
-    [SerializeField] PhotonView view;
+    [SerializeField] List<Color> colorList;
+
     [SerializeField] PlayerInput input;
     [SerializeField] float moveSpeed;
+
+    [SerializeField] int fireCount;
 
     private Vector3 moveDir;
 
     private void Awake()
     {
-        if (view.IsMine == false)
+        if (photonView.IsMine == false)
         {
             Destroy(input);
         }
+
+        SetPlayerColor();
     }
 
     private void Update()
-    {        
+    {
         transform.Translate(moveDir * moveSpeed * Time.deltaTime);
     }
     private void OnMove(InputValue value)
     {
         moveDir.x = value.Get<Vector2>().x;
         moveDir.z = value.Get<Vector2>().y;
+    }
+
+    private void OnFire(InputValue value)
+    {
+        fireCount++;
+    }
+
+    private void SetPlayerColor()
+    {
+        int playerNumber = photonView.Owner.GetPlayerNumber();
+        if (colorList == null || colorList.Count <= playerNumber) return;
+
+        Renderer render = GetComponent<Renderer>();
+        render.material.color = colorList[playerNumber];
+
+        if (photonView.IsMine)
+        {
+            render.material.color = Color.green;
+        }
+    }
+
+    public void OnPhotonSeriaLizeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.IsWriting)
+        {
+            stream.SendNext(fireCount);
+        }
+        else
+        {
+            fireCount = (int)stream.ReceiveNext();
+        }
     }
 }
